b94c278b1982220a5270e29215bc2c1faa92670b
Sai Surya Teja <saisuryateja3@gmail.com>
2024-01-10 01:06:13 +0530

    Handling POST Request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,18 @@ import { comments } from "./data";
 export const GET = async () => {
   return Response.json(comments);
 };
+
+export const POST = async (request: Request) => {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    comment: comment.comment,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+};
