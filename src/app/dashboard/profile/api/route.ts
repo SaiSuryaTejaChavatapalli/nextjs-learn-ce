46fcf9e783598d3b653f8c6038264c5347297144
Sai Surya Teja <saisuryateja3@gmail.com>
2024-01-22 11:55:07 +0530

    Cookies in route handlers

--- src/app/dashboard/profile/api/route.ts
+++ src/app/dashboard/profile/api/route.ts
@@ -1,22 +1,27 @@
 import { type NextRequest } from "next/server";
 
-export function GET(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  console.log(requestHeaders.get("Authorization"));
+// export function GET(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   console.log(requestHeaders.get("Authorization"));
 
-  const cookie = request.cookies.get("theme");
-  console.log("Cookie", cookie);
-  return new Response("<h1>Dashboard/profile - route</h1>", {
-    headers: {
-      "Content-Type": "text/html",
-      "Set-Cookie": "theme=dark",
-    },
-  });
-}
-
-// import { headers } from "next/headers";
-// export function GET() {
-//   const reqHeaders = headers();
-//   console.log("From next headers function:", reqHeaders.get("Authorization"));
-//   return new Response("Dashboard/profile - route");
+//   const cookie = request.cookies.get("theme");
+//   console.log("Cookie", cookie);
+//   return new Response("<h1>Dashboard/profile - route</h1>", {
+//     headers: {
+//       "Content-Type": "text/html",
+//       "Set-Cookie": "theme=dark",
+//     },
+//   });
 // }
+
+import { headers, cookies } from "next/headers";
+export function GET() {
+  const reqHeaders = headers();
+  cookies().set("resultsPerPage", "20");
+  console.log(
+    "Cookies from cookies function next:",
+    cookies().get("resultsPerPage")
+  );
+  console.log("From next headers function:", reqHeaders.get("Authorization"));
+  return new Response("Dashboard/profile - route");
+}
