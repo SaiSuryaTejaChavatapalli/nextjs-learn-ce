c007dc2ef9c5c21983adf5351c29a603650a6173
Sai Surya Teja <saisuryateja3@gmail.com>
2024-01-22 11:41:03 +0530

    Headers in Route handlers

--- src/app/dashboard/profile/api/route.ts
+++ src/app/dashboard/profile/api/route.ts
@@ -1,3 +1,22 @@
-export function GET() {
-  return new Response("Dashboard/profile - route");
+import { type NextRequest } from "next/server";
+
+export function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
+
+  const cookie = request.cookies.get("theme");
+  console.log("Cookie", cookie);
+  return new Response("<h1>Dashboard/profile - route</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
+
+// import { headers } from "next/headers";
+// export function GET() {
+//   const reqHeaders = headers();
+//   console.log("From next headers function:", reqHeaders.get("Authorization"));
+//   return new Response("Dashboard/profile - route");
+// }
